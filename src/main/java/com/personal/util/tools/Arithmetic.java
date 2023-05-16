c101f17cbc28b889038ccd43ae74374a77e8fb1b
原帅帅 <9148028+shuai-yuan@user.noreply.gitee.com>
2023-05-16 11:57:02 +0800

    更新数学运算工具类

--- src/main/java/com/personal/util/tools/Arithmetic.java
+++ src/main/java/com/personal/util/tools/Arithmetic.java
@@ -2,6 +2,9 @@ package com.personal.util.tools;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.regex.Pattern;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
 
 /**
  * 精确地浮点数运算
@@ -9,16 +12,29 @@ import java.math.RoundingMode;
  * @author yuanss
  */
 public class Arithmetic {
-    /** 默认除法运算精度 */
+
+    /**
+     * 默认除法运算精度
+     */
     private static final int DEF_DIV_SCALE = 10;
 
-    /** 这个类不能实例化 */
+    private static final String TEN_THOUSAND_UNIT = "万";
+    private static final String HUNDRED_MILLION_UNIT = "亿";
+
+    private static final BigDecimal TEN_HUNDRED_THOUSAND = new BigDecimal(10000);
+    private static final BigDecimal ONE_HUNDRED_MILLION = new BigDecimal(100000000);
+    private static final BigDecimal TEN_THOUSAND = new BigDecimal(10000);
+
+    /**
+     * 这个类不能实例化
+     */
     private Arithmetic() {
 
     }
 
     /**
      * 提供精确的加法运算
+     *
      * @param v1 被加数
      * @param v2 加数
      * @return 两个参数的和
@@ -30,7 +46,38 @@ public class Arithmetic {
     }
 
     /**
+     * 提供精确的加法运算
+     *
+     * @param v1 被加数
+     * @param v2 加数
+     * @return 两个参数的和
+     */
+    public static BigDecimal add(String v1, String v2) {
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.add(b2);
+    }
+
+    /**
+     * 提供精确的加法运算
+     *
+     * @param v1    被加数
+     * @param v2    加数
+     * @param scale 保留scale 位小数
+     * @return 两个参数的和
+     */
+    public static String add(String v1, String v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.add(b2).setScale(scale, RoundingMode.HALF_UP).toString();
+    }
+
+    /**
      * 提供精确的减法运算
+     *
      * @param v1 被减数
      * @param v2 减数
      * @return 两个参数的差
@@ -42,7 +89,38 @@ public class Arithmetic {
     }
 
     /**
+     * 提供精确的减法运算。
+     *
+     * @param v1 被减数
+     * @param v2 减数
+     * @return 两个参数的差
+     */
+    public static BigDecimal sub(String v1, String v2) {
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.subtract(b2);
+    }
+
+    /**
+     * 提供精确的减法运算
+     *
+     * @param v1    被减数
+     * @param v2    减数
+     * @param scale 保留scale 位小数
+     * @return 两个参数的差
+     */
+    public static String sub(String v1, String v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.subtract(b2).setScale(scale, RoundingMode.HALF_UP).toString();
+    }
+
+    /**
      * 提供精确的乘法运算
+     *
      * @param v1 被乘数
      * @param v2 乘数
      * @return 两个参数的积
@@ -54,8 +132,52 @@ public class Arithmetic {
     }
 
     /**
-     * 提供（相对）精确的除法运算，当发生除不尽的情况时，精确到
-     * 小数点以后10位，以后的数字四舍五入
+     * 提供精确的乘法运算
+     *
+     * @param v1 被乘数
+     * @param v2 乘数
+     * @return 两个参数的积
+     */
+    public static BigDecimal mul(String v1, String v2) {
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.multiply(b2);
+    }
+
+    /**
+     * 提供精确的乘法运算
+     *
+     * @param v1    被乘数
+     * @param v2    乘数
+     * @param scale 保留scale 位小数
+     * @return 两个参数的积
+     */
+    public static double mul(double v1, double v2, int scale) {
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return round(b1.multiply(b2).doubleValue(), scale);
+    }
+
+    /**
+     * 提供精确的乘法运算
+     *
+     * @param v1    被乘数
+     * @param v2    乘数
+     * @param scale 保留scale 位小数
+     * @return 两个参数的积
+     */
+    public static String mul(String v1, String v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.multiply(b2).setScale(scale, RoundingMode.HALF_UP).toString();
+    }
+
+    /**
+     * 提供（相对）精确的除法运算，当发生除不尽的情况时，精确到 小数点以后10位，以后的数字四舍五入
+     *
      * @param v1 被除数
      * @param v2 除数
      * @return 两个参数的商
@@ -65,10 +187,10 @@ public class Arithmetic {
     }
 
     /**
-     * 提供（相对）精确的除法运算，当发生除不尽的情况时，由scale参数指定
-     * 精度，以后的数字四舍五入
-     * @param v1 被除数
-     * @param v2 除数
+     * 提供（相对）精确的除法运算，当发生除不尽的情况时，由scale参数指定 精度，以后的数字四舍五入
+     *
+     * @param v1    被除数
+     * @param v2    除数
      * @param scale 表示需要精确到小数点以后几位
      * @return 两个参数的商
      */
@@ -85,17 +207,125 @@ public class Arithmetic {
     }
 
     /**
+     * 提供（相对）精确的除法运算。当发生除不尽的情况时，由scale参数指
+     * 定精度，以后的数字四舍五入
+     *
+     * @param v1    被除数
+     * @param v2    除数
+     * @param scale 表示需要精确到小数点以后几位
+     * @return 两个参数的商
+     */
+    public static String div(String v1, String v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.divide(b2, scale, RoundingMode.HALF_UP).toString();
+    }
+
+    /**
      * 提供精确的小数位四舍五入处理
-     * @param v 需要四舍五入的数字
+     *
+     * @param v     需要四舍五入的数字
      * @param scale 小数点后保留几位
      * @return 四舍五入后的结果
      */
     public static double round(double v, int scale) {
+        if (scale < 0) {throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b = new BigDecimal(Double.toString(v));
+        return b.setScale(scale, RoundingMode.HALF_UP).doubleValue();
+    }
+
+    /**
+     * 提供精确的小数位四舍五入处理
+     *
+     * @param v     需要四舍五入的数字
+     * @param scale 小数点后保留几位
+     * @return 四舍五入后的结果
+     */
+    public static String round(String v, int scale) {
         if (scale < 0) {
             throw new IllegalArgumentException("The scale must be a positive integer or zero");
         }
         BigDecimal b = new BigDecimal(v);
-        BigDecimal one = new BigDecimal("1");
-        return b.divide(one, scale, RoundingMode.HALF_UP).doubleValue();
+        return b.setScale(scale, RoundingMode.HALF_UP).toString();
+    }
+
+    /**
+     * 取余数
+     *
+     * @param v1    被除数
+     * @param v2    除数
+     * @param scale 小数点后保留几位
+     * @return 余数
+     */
+    public static String remainder(String v1, String v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        return b1.remainder(b2).setScale(scale, RoundingMode.HALF_UP).toString();
+    }
+
+    /**
+     * 取余数  BigDecimal
+     *
+     * @param v1    被除数
+     * @param v2    除数
+     * @param scale 小数点后保留几位
+     * @return 余数
+     */
+    public static BigDecimal remainder(BigDecimal v1, BigDecimal v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        return v1.remainder(v2).setScale(scale, RoundingMode.HALF_UP);
+    }
+
+    /**
+     * 比较大小
+     *
+     * @param v1 被比较数
+     * @param v2 比较数
+     * @return 如果v1 大于v2 则 返回true 否则false
+     */
+    public static boolean compare(String v1, String v2) {
+        BigDecimal b1 = new BigDecimal(v1);
+        BigDecimal b2 = new BigDecimal(v2);
+        int bj = b1.compareTo(b2);
+        return bj > 0;
+    }
+
+    /**
+     * 过万/亿自动转换单位 返回格式 数值_单位
+     * @param var 待转换参数
+     * @param scale 保留位数
+     * @return 带单位的数字字符串
+     */
+    public static String amountConversion(String var, boolean isTenThousand, @Nullable Integer scale) {
+        // 参数校验
+        Assert.notNull(var, "The var cannot be empty");
+        Pattern pattern = Pattern.compile("(^[+-]?[0-9]+)|(^[+-]?[0-9]+\\.[0-9]+)");
+        if (!pattern.matcher(var).matches()) {
+            throw new IllegalArgumentException("var must be number");
+        }
+
+        BigDecimal amount = new BigDecimal(var);
+        if (scale == null) {    // 默认保留4位
+            scale = 4;
+        }
+
+        if (amount.abs().compareTo(TEN_HUNDRED_THOUSAND) < 0) {
+            //如果小于1万
+            return amount.stripTrailingZeros().setScale(scale, RoundingMode.HALF_UP).toPlainString() + "_" + (isTenThousand ? TEN_THOUSAND_UNIT : "");
+        }
+        if (amount.abs().compareTo(ONE_HUNDRED_MILLION) < 0) {
+            //如果大于1万小于1亿
+            return amount.divide(TEN_THOUSAND, scale, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString() + "_" + (isTenThousand ? HUNDRED_MILLION_UNIT : TEN_THOUSAND_UNIT);
+        }
+        return amount.divide(ONE_HUNDRED_MILLION, scale, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString() + "_" + (isTenThousand ? TEN_THOUSAND_UNIT + HUNDRED_MILLION_UNIT : HUNDRED_MILLION_UNIT);
     }
 }
